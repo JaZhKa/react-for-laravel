3370a8fd6ddeba905b2841c7d51aa39ddee954c2
Yakov Lemenkov <yakoff4@gmail.com>
2023-12-29 17:47:55 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import {Link, Route, Routes} from 'react-router-dom';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return <div>
+    <nav>
+      <Link to='/login'>Login</Link>
+      <Link to='/register'>Register</Link>
+    </nav>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  </div>;
+}
+
+export default App;
