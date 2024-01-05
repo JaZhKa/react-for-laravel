b45b4da45031748567ed3204014023e2c0ff8916
Yakov Lemenkov <yakoff4@gmail.com>
2024-01-05 21:03:40 +0300

    add athor to post

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
+import useAuthContext from "../context/AuthContext";
 
 function NewPost() {
+  const { user } = useAuthContext();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [image, setImage] = useState("");
@@ -39,13 +41,14 @@ function NewPost() {
       tags: tags.map((tag) => ({
         id: parseInt(tag),
       })),
+      user_id: user.id,
     };
     await axios.post("api/posts", data, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("JWT")}`,
       },
     });
-    navigate('/posts')
+    navigate("/posts");
   };
 
   useEffect(() => {
@@ -55,56 +58,56 @@ function NewPost() {
   return (
     <div>
       <form onSubmit={handleSubmit} className="space-y-8">
-        <div className='mb-3'>
-          <label className='block text-gray-500 font-bold text-start mb-1 pr-4'>
+        <div className="mb-3">
+          <label className="mb-1 block pr-4 text-start font-bold text-gray-500">
             Title
           </label>
           <input
-            name='title'
-            type='text'
-            id='title'
-            placeholder='Title'
+            name="title"
+            type="text"
+            id="title"
+            placeholder="Title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           />
         </div>
-        <div className='mb-3'>
-          <label className='block text-gray-500 font-bold text-start mb-1 pr-4'>
+        <div className="mb-3">
+          <label className="mb-1 block pr-4 text-start font-bold text-gray-500">
             Content
           </label>
           <textarea
-            name='content'
-            id='content'
-            rows='3'
-            placeholder='Content'
+            name="content"
+            id="content"
+            rows="3"
+            placeholder="Content"
             onChange={(e) => setContent(e.target.value)}
             value={content}
-            className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           ></textarea>
         </div>
-        <div className='mb-3'>
-          <label className='block text-gray-500 font-bold text-start mb-1 pr-4'>
+        <div className="mb-3">
+          <label className="mb-1 block pr-4 text-start font-bold text-gray-500">
             Image
           </label>
           <input
-            name='image'
-            type='img'
-            id='image'
-            placeholder='image.jpeg'
+            name="image"
+            type="img"
+            id="image"
+            placeholder="image.jpeg"
             value={image}
             onChange={(e) => setImage(e.target.value)}
-            className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+            className="w-full appearance-none rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           />
         </div>
         <div>
-          <label className='block text-gray-500 font-bold text-start mb-1 pr-4'>
+          <label className="mb-1 block pr-4 text-start font-bold text-gray-500">
             Category
           </label>
           <select
             value={category}
             onChange={(e) => setCategory(e.target.value)}
-            className='bg-gray-200 appearance-auto border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 outline'
+            className="w-full appearance-auto rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           >
             {isLoaded &&
               categories.map((category) => (
@@ -115,7 +118,7 @@ function NewPost() {
           </select>
         </div>
         <div>
-          <label className='block text-gray-500 font-bold text-start mb-1 pr-4'>
+          <label className="mb-1 block pr-4 text-start font-bold text-gray-500">
             Tags
           </label>
           <select
@@ -125,7 +128,7 @@ function NewPost() {
               const value = options.map((option) => option.value);
               setTag(value);
             }}
-            className='bg-gray-200 appearance-auto border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 outline'
+            className="w-full appearance-auto rounded border-2 border-gray-200 bg-gray-200 px-4 py-2 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
           >
             {isLoaded &&
               allTags.map((tag) => (
@@ -137,8 +140,8 @@ function NewPost() {
         </div>
         <div className="flex justify-start">
           <button
-             className='shadow bg-gray-700 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
-            type='submit'
+            className="focus:shadow-outline rounded bg-gray-700 px-4 py-2 font-bold text-white shadow hover:bg-gray-400 focus:outline-none"
+            type="submit"
           >
             Add
           </button>
